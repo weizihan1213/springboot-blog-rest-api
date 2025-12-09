7ae6d6c6eb90203195ac527f5046bc07aa918803
Zihan Wei <1319651561wzh@gmail.com>
2025-12-08 22:50:50 -0600

    whitelisted the login users.

--- src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import java.net.PasswordAuthentication;
 
+@Service
 public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
