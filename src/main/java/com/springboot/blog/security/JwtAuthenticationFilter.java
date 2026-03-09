e309d702113f9f2189b85526875f3b1464fa7cf3
Zihan Wei <1319651561wzh@gmail.com>
2026-03-08 18:22:44 -0700

    feat: create JwtAuthenticationFilter file for adding jwt token filter

---
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationFilter.java
@@ -0,0 +1,79 @@
+package com.springboot.blog.security;
+
+import ch.qos.logback.core.util.StringUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserDetailsService userDetailsService;
+
+    /**
+     * Filter that authenticates requests based on JWT token.
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        // Get jwt token from the HTTP request
+        String token = getTokenFromRequest(request);
+
+        // Validate the jwt token
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+
+            // Get username from token
+            String username = this.jwtTokenProvider.getUserName(token);
+
+            // Load the user associated with token
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Retrieve the JWT token from the HTTP request
+     *
+     * @param request
+     * @return
+     */
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return  bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+}
