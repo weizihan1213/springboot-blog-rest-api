4b7c5d9b0d7510d9c6609f6a8f65cbbeae3c0628
Zihan Wei <1319651561wzh@gmail.com>
2026-03-11 21:22:07 -0700

    feat: create category DTO and repository file

---
+++ src/main/java/com/springboot/blog/payload/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.springboot.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
