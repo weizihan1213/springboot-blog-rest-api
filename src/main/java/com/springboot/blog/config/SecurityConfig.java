b6fcaba9cb3d06faaa440478b82cc1fb9f8eed96
Zihan Wei <1319651561wzh@gmail.com>
2026-03-17 18:16:23 -0700

    feat: update the spring doc package version and allow authenticated access to swagger endpoints

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -57,6 +57,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 .requestMatchers("api/auth/**").permitAll()
+                                .requestMatchers("swagger-ui/**").permitAll()
+                                .requestMatchers("v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                 ).exceptionHandling( exception -> exception
                         .authenticationEntryPoint( authenticationEntryPoint )
