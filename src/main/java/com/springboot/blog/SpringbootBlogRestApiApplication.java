c92ef36ae04566c901596dcec0a1b5527e6d087a
Zihan Wei <1319651561wzh@gmail.com>
2026-03-18 21:48:06 -0700

    feat: add swagger API definition for the blog app

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,5 +1,10 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +13,25 @@ import org.springframework.context.annotation.Bean;
 
 @Data
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot BLOG App REST APIs",
+				description = "Blog Platform REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Eric",
+						email = "ericwei@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/weizihan1213/springboot-blog-rest-api"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 	@Bean
 	public ModelMapper modelMapper(){
